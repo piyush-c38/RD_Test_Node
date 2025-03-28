fa8afcc281dc5e9d87bab18d9dcf1f21cb9bfecb
piyush-c38 <chandrakarpiyush36@gmail.com>
2025-03-28 15:22:32 +0530

    FEAT: adding ejs forms

--- src/routes/person.routes.js
+++ src/routes/person.routes.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const Person = require("../models/person.model");
 const router = express.Router();
-
 const {
-  showPersonList, deletePerson, createPerson, editPerson
+  showPersonList,
+  deletePerson,
+  createPerson,
+  editPerson,
 } = require("../controllers/person.controller");
 
+// CRUD Routes
+router.route("/").get(showPersonList).post(createPerson);
+router.route("/edit/:id").get(async (req, res) => {
+  const person = await Person.findById(req.params.id);
+  if (!person) {
+    return res.status(404).json({ message: "Person not found" });
+  }
+  res.render('edit', { person });
+});
+router.route("/create").get((req, res) => res.render('create'));
+router.route("/:id").put(editPerson);
+router.route("/delete/:id").delete(deletePerson);
 
-router.route("/").post(createPerson).get(showPersonList);
-router.route("/:id").delete(deletePerson).put(editPerson);
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const connectDb = require("./config/db");
 const errorHandler = require("./middleware/errorHandler");
+const methodOverride = require('method-override');
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/person", require("./routes/person.routes"));
+app.use(express.urlencoded({ extended: true }));
+const path = require('path');
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views')); // Ensure this path points to your views folder
+
+app.use(methodOverride('_method'));
+app.use("/person", require("./routes/person.routes"));
 app.use(errorHandler);
 
 app.listen(port, () => {
